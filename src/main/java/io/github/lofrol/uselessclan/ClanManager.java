a9ee98ee1cdea0d710bffd2fbb3d6838d0a12090
LoFRoL <33431126+LoFRoL@users.noreply.github.com>
2022-10-26 00:38:39 +0400

    Added base functions for serialization

--- src/main/java/io/github/lofrol/uselessclan/ClanManager.java
+++ src/main/java/io/github/lofrol/uselessclan/ClanManager.java
@@ -1,4 +1,29 @@
 package io.github.lofrol.uselessclan;
 
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.io.File;
+import java.util.*;
+
 public class ClanManager {
+    private UselessClan OwnerPlugin;
+    public ClanManager(UselessClan owner) {
+        ServerClans = new HashMap<String, Clan>();
+
+        OwnerPlugin = owner;
+
+    }
+
+    public void serializeClans() {
+        for (File tempFile : OwnerPlugin.getDataFolder().listFiles()) {
+            tempFile.list();
+        }
+    }
+    public void deserializeClans() {
+
+    }
+
+    Map<String, Clan> ServerClans;
+
 }
