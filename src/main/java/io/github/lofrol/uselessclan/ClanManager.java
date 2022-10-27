fb51c19dc94a88a19848e395fbd5e21dce420a46
LoFRoL <33431126+LoFRoL@users.noreply.github.com>
2022-10-28 00:51:38 +0400

    Update ClanManager.java

--- src/main/java/io/github/lofrol/uselessclan/ClanManager.java
+++ src/main/java/io/github/lofrol/uselessclan/ClanManager.java
@@ -3,8 +3,11 @@ package io.github.lofrol.uselessclan;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import javax.swing.filechooser.FileSystemView;
 import java.io.File;
+import java.io.IOException;
 import java.util.*;
+import java.util.logging.Level;
 
 public class ClanManager {
     private UselessClan OwnerPlugin;
@@ -15,9 +18,17 @@ public class ClanManager {
 
     }
 
-    public void serializeClans() {
+    public void serializeClans() throws IOException {
+        boolean isCreateNew = true;
         for (File tempFile : OwnerPlugin.getDataFolder().listFiles()) {
-            tempFile.list();
+            if (tempFile.getName().equalsIgnoreCase("clan.yml")) {
+                OwnerPlugin.getLogger().log(Level.INFO, "Finded clan.yml");
+                isCreateNew = false;
+                break;
+            }
+        }
+        if (isCreateNew) {
+           File tempfile = new File(OwnerPlugin.getDataFolder(), "ClanConfig");
         }
     }
     public void deserializeClans() {
