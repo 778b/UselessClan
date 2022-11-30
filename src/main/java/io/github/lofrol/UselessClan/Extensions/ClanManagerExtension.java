22ade690de4d8e2e48b11789da09d3934e1cac56
778b <kklokov@inbox.ru>
2022-12-01 02:28:31 +0400

    Added defautl constructor

--- src/main/java/io/github/lofrol/UselessClan/Extensions/ClanManagerExtension.java
+++ src/main/java/io/github/lofrol/UselessClan/Extensions/ClanManagerExtension.java
@@ -3,6 +3,10 @@ package io.github.lofrol.UselessClan.Extensions;
 import io.github.lofrol.UselessClan.ClanObjects.Clan;
 
 public class ClanManagerExtension {
+     public ClanManagerExtension() {
+
+     }
+
      public void CalculateClanLevel(Clan ClanToCalculate) {
          // no logic for default
      }
