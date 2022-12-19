9b7d1bbb6a83ffa023165b485fd513149a19ac23
778b <kklokov@inbox.ru>
2022-12-20 02:33:21 +0400

    Update ChatSender.java

--- src/main/java/io/github/lofrol/UselessClan/Utils/ChatSender.java
+++ src/main/java/io/github/lofrol/UselessClan/Utils/ChatSender.java
@@ -1,6 +1,7 @@
 package io.github.lofrol.UselessClan.Utils;
 
 import org.bukkit.ChatColor;
+import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 public class ChatSender {
@@ -8,4 +9,7 @@ public class ChatSender {
         recipient.sendMessage(ChatColor.translateAlternateColorCodes('&', String.format("&f[&8%s&f] &b%s", prefix, message)));
     }
 
+    public static void MessageTo(CommandSender recipient, String prefix, String message) {
+        recipient.sendMessage(ChatColor.translateAlternateColorCodes('&', String.format("&f[&8%s&f] &b%s", prefix, message)));
+    }
 }
