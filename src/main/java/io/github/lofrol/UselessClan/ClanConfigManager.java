98689fcdc53924e29249cdd4ee250c67f1d3c4a0
778b <kklokov@inbox.ru>
2023-01-06 05:52:36 +0400

    Refactored config manager

--- src/main/java/io/github/lofrol/UselessClan/ClanConfigManager.java
+++ src/main/java/io/github/lofrol/UselessClan/ClanConfigManager.java
@@ -1,13 +1,14 @@
 package io.github.lofrol.UselessClan;
 
+import com.sk89q.worldedit.util.collection.DoubleArrayList;
 import io.github.lofrol.UselessClan.ClanObjects.EClanRole;
+import io.github.lofrol.UselessClan.Configurations.ClanConfigConfiguration;
 import org.bukkit.configuration.file.FileConfiguration;
-import org.bukkit.configuration.file.YamlConfiguration;
 import org.checkerframework.checker.nullness.qual.Nullable;
 import org.jetbrains.annotations.NotNull;
 
+import java.io.File;
 import java.io.IOException;
-import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 import java.util.logging.Level;
@@ -17,253 +18,46 @@ import static java.util.Map.entry;
 public class ClanConfigManager {
     private final UselessClan OwnerPlugin;
 
-    private ClanPluginData ClanConfig;
+    private final ClanConfigConfiguration ClanConfig;
 
     public ClanConfigManager(UselessClan owner) {
         OwnerPlugin = owner;
 
         ClanConfig = readConfigValues();
-        if (ClanConfig == null) {
-            setupDefaultConfig();
-            ClanConfig = loadDefaultConfig();
+    }
+
+    private @NotNull ClanConfigConfiguration readConfigValues() {
+        FileConfiguration tempConfig = OwnerPlugin.getConfig();
+        var tempConfiguration = ClanConfigConfiguration.tryLoadDifferentConfig(tempConfig);
+        if (tempConfiguration == null) {
             OwnerPlugin.getLogger().log(Level.SEVERE, "Config is corrupted or invalid, initialized new");
+            return setupDefaultConfig();
         }
-        else {
-            OwnerPlugin.getLogger().log(Level.INFO, "Config was load");
+        OwnerPlugin.getLogger().log(Level.INFO, "Config was load");
+        return tempConfiguration;
+    }
+
+
+    public @NotNull ClanConfigConfiguration setupDefaultConfig() {
+        var tempClanConfig = new ClanConfigConfiguration();
+        File tempFolder = OwnerPlugin.getDataFolder();
+        UselessClan.getSerilManager().checkFolderOrCreate(tempFolder);
+        File newConfigFile = new File(tempFolder, "config.yml");
+
+        try {
+            tempClanConfig.save(newConfigFile);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
-    }
 
-    public @Nullable ClanPluginData readConfigValues() {
-        FileConfiguration tempConfig = OwnerPlugin.getConfig();
-
-        String LocalizationKey = tempConfig.getString("Localization");
-        if (LocalizationKey == null) return null;
-        boolean NeedCalculateClanLevels = tempConfig.getBoolean("NeedCalculateClanLevels");
-
-        int backupDelay = tempConfig.getInt("Delay.ClanBackup");
-        int autoSaveDelay = tempConfig.getInt("Delay.AutoSave");
-        int calcClanLvlDelay = tempConfig.getInt("Delay.CalculateClanLevel");
-        int calcClanTopDelay = tempConfig.getInt("Delay.CalculateClansTop");
-
-        EClanRole MinRoleCanSethome = EClanRole.fromInt(tempConfig.getInt("ClanSettings.MinRoleCanSethome"));
-        if (MinRoleCanSethome == EClanRole.NONE) return null;
-        EClanRole MinRoleCanWithdraw = EClanRole.fromInt(tempConfig.getInt("ClanSettings.MinRoleCanWithdraw"));
-        if (MinRoleCanWithdraw == EClanRole.NONE) return null;
-        EClanRole MinRoleCanKick = EClanRole.fromInt(tempConfig.getInt("ClanSettings.MinRoleCanKick"));
-        if (MinRoleCanKick == EClanRole.NONE) return null;
-        EClanRole MinRoleCanAccept = EClanRole.fromInt(tempConfig.getInt("ClanSettings.MinRoleCanAccept"));
-        if (MinRoleCanAccept == EClanRole.NONE) return null;
-        EClanRole DefaultJoinRole = EClanRole.fromInt(tempConfig.getInt("ClanSettings.DefaultJoinRole"));
-        if (DefaultJoinRole == EClanRole.NONE) return null;
-
-        double FirstClanMoney = tempConfig.getDouble("DefaultClanSettings.FirstClanMoney");
-        double MoneyToCreate = tempConfig.getDouble("DefaultClanSettings.MoneyToCreateClan");
-        int StartClanLevel = tempConfig.getInt("DefaultClanSettings.StartClanLevel");
-        List<String> ClanLevelsColors = tempConfig.getStringList("DefaultClanSettings.ClanLevelsColors");
-
-        return new ClanPluginData(LocalizationKey, NeedCalculateClanLevels,
-                backupDelay, autoSaveDelay, calcClanLvlDelay, calcClanTopDelay,
-                MinRoleCanSethome, MinRoleCanWithdraw, MinRoleCanKick, MinRoleCanAccept,
-                DefaultJoinRole, FirstClanMoney, MoneyToCreate, StartClanLevel, ClanLevelsColors);
-    }
-
-    private @NotNull ClanPluginData loadDefaultConfig() {
-        return new ClanPluginData(
-                LocalizationDefault,
-                NeedCalculateClanLevelsDefault,
-                DelayClanBackupDefault,
-                DelayAutoSaveDefault,
-                DelayCalculateClanLevelDefault,
-                DelayCalculateClansTopDefault,
-
-                EClanRole.fromInt(ClanSettingsMinRoleCanSethomeDefault),
-                EClanRole.fromInt(ClanSettingsMinRoleCanWithdrawDefault),
-                EClanRole.fromInt(ClanSettingsMinRoleCanKickDefault),
-                EClanRole.fromInt(ClanSettingsMinRoleCanAcceptDefault),
-                EClanRole.fromInt(ClanSettingsDefaultJoinRoleDefault),
-
-                DefaultClanSettingsFirstClanMoneyDefault,
-                DefaultClanSettingsMoneyToCreateDefault,
-                DefaultClanSettingsStartClanLevelDefault,
-                ClanLevelsColorsDefault);
-    }
-
-    public void setupDefaultConfig() {
-        FileConfiguration tempConfig = OwnerPlugin.getConfig();
-
-        tempConfig.set("Localization", LocalizationDefault);
-        tempConfig.set("NeedCalculateClanLevels", NeedCalculateClanLevelsDefault);
-
-        tempConfig.set("Delay.ClanBackup", DelayClanBackupDefault);
-        tempConfig.set("Delay.AutoSave", DelayAutoSaveDefault);
-        tempConfig.set("Delay.CalculateClanLevel", DelayCalculateClanLevelDefault);
-        tempConfig.set("Delay.CalculateClansTop", DelayCalculateClansTopDefault);
-
-        tempConfig.set("ClanSettings.MinRoleCanSethome", ClanSettingsMinRoleCanSethomeDefault);
-        tempConfig.set("ClanSettings.MinRoleCanWithdraw", ClanSettingsMinRoleCanWithdrawDefault);
-        tempConfig.set("ClanSettings.MinRoleCanKick", ClanSettingsMinRoleCanKickDefault);
-        tempConfig.set("ClanSettings.MinRoleCanAccept", ClanSettingsMinRoleCanAcceptDefault);
-        tempConfig.set("ClanSettings.DefaultJoinRole", ClanSettingsDefaultJoinRoleDefault);
-
-        tempConfig.set("DefaultClanSettings.FirstClanMoney", DefaultClanSettingsFirstClanMoneyDefault);
-        tempConfig.set("DefaultClanSettings.MoneyToCreateClan", DefaultClanSettingsMoneyToCreateDefault);
-        tempConfig.set("DefaultClanSettings.StartClanLevel", DefaultClanSettingsStartClanLevelDefault);
-        tempConfig.set("DefaultClanSettings.ClanLevelsColors", ClanLevelsColorsDefault);
-
-        OwnerPlugin.saveConfig();
+        return tempClanConfig;
     }
 
 
     /*
      *   Getters
      */
-    public ClanPluginData getClanConfig() {
+    public ClanConfigConfiguration getClanConfig() {
         return ClanConfig;
     }
-
-
-    /*
-     *   Default static values
-     */
-    public static class ClanPluginData {
-        private final String LocalizationKey;
-        private final boolean NeedCalculateClanLevels;
-
-        private final int backupDelay;
-        private final int autoSaveDelay;
-        private final int calcClanLvlDelay;
-        private final int calcClanTopDelay;
-
-        private final EClanRole MinRoleCanSethome;
-        private final EClanRole MinRoleCanWithdraw;
-        private final EClanRole MinRoleCanKick;
-        private final EClanRole MinRoleCanAccept;
-        private final EClanRole DefaultJoinRole;
-
-        private final double FirstClanMoney;
-        private final double MoneyToCreateClan;
-        private final int StartClanLevel;
-        private final List<String> ClanLevelsColors;
-
-        public ClanPluginData(String loc, boolean needCalc, int backup, int autosave, int calcLvl, int calcTop,
-                              EClanRole sethomeRole, EClanRole withdrawRole, EClanRole kickRole, EClanRole acceptRole,
-                              EClanRole defaultRole, double firstMoney, double moneyToCreate, int startLevel, List<String> clanLevelsColors) {
-            LocalizationKey = loc;
-            NeedCalculateClanLevels = needCalc;
-
-            backupDelay = backup;
-            autoSaveDelay = autosave;
-            calcClanLvlDelay = calcLvl;
-            calcClanTopDelay = calcTop;
-
-            MinRoleCanSethome = sethomeRole;
-            MinRoleCanWithdraw = withdrawRole;
-            MinRoleCanKick = kickRole;
-            MinRoleCanAccept = acceptRole;
-            DefaultJoinRole = defaultRole;
-
-            FirstClanMoney = firstMoney;
-            MoneyToCreateClan = moneyToCreate;
-            StartClanLevel = startLevel;
-            ClanLevelsColors = clanLevelsColors;
-
-        }
-
-        public String getLocalizationKey() {
-            return LocalizationKey;
-        }
-
-        public boolean isNeedCalculateClanLevels() {
-            return NeedCalculateClanLevels;
-        }
-
-        public int getBackupDelay() {
-            return backupDelay;
-        }
-
-        public int getAutoSaveDelay() {
-            return autoSaveDelay;
-        }
-
-        public int getCalcClanLvlDelay() {
-            return calcClanLvlDelay;
-        }
-
-        public int getCalcClanTopDelay() {
-            return calcClanTopDelay;
-        }
-
-        public EClanRole getMinRoleCanSethome() {
-            return MinRoleCanSethome;
-        }
-
-        public EClanRole getMinRoleCanWithdraw() {
-            return MinRoleCanWithdraw;
-        }
-
-        public EClanRole getMinRoleCanKick() {
-            return MinRoleCanKick;
-        }
-
-        public EClanRole getMinRoleCanAccept() {
-            return MinRoleCanAccept;
-        }
-
-        public EClanRole getDefaultJoinRole() {
-            return DefaultJoinRole;
-        }
-
-        public double getFirstClanMoney() {
-            return FirstClanMoney;
-        }
-        public double getMoneyToCreateClan() { return MoneyToCreateClan; }
-        public int getStartClanLevel() {
-            return StartClanLevel;
-        }
-
-        public List<String> getClanLevelsColors() {
-            return ClanLevelsColors;
-        }
-
-    }
-
-    private static final String LocalizationDefault = "en-US";
-    private static final boolean NeedCalculateClanLevelsDefault = true;
-    private static final int DelayClanBackupDefault = 864000;
-    private static final int DelayAutoSaveDefault = 24000;
-    private static final int DelayCalculateClanLevelDefault = 12000;
-    private static final int DelayCalculateClansTopDefault = 12000;
-    private static final int ClanSettingsMinRoleCanSethomeDefault = 4;
-    private static final int ClanSettingsMinRoleCanWithdrawDefault = 2;
-    private static final int ClanSettingsMinRoleCanKickDefault = 3;
-    private static final int ClanSettingsMinRoleCanAcceptDefault = 3;
-    private static final int ClanSettingsDefaultJoinRoleDefault = 1;
-    private static final double DefaultClanSettingsFirstClanMoneyDefault = 0;
-    private static final double DefaultClanSettingsMoneyToCreateDefault = 10000;
-    private static final int DefaultClanSettingsStartClanLevelDefault = 0;
-    private static final List<String> ClanLevelsColorsDefault =
-            List.of("&f", "&a", "&2", "&3", "&9", "&1", "&e", "&6", "&d", "&5", "&0");
-
-    private static final Map<String, Object> DefaultUselessClanConfiguration = Map.ofEntries(
-            entry("Localization", LocalizationDefault),
-            entry("NeedCalculateClanLevels", NeedCalculateClanLevelsDefault),
-
-            entry("Delay.ClanBackup", DelayClanBackupDefault),
-            entry("Delay.AutoSave", DelayAutoSaveDefault),
-            entry("Delay.CalculateClanLevel", DelayCalculateClanLevelDefault),
-            entry("Delay.CalculateClansTop", DelayCalculateClansTopDefault),
-
-            entry("ClanSettings.MinRoleCanSethome", ClanSettingsMinRoleCanSethomeDefault),
-            entry("ClanSettings.MinRoleCanWithdraw", ClanSettingsMinRoleCanWithdrawDefault),
-            entry("ClanSettings.MinRoleCanKick", ClanSettingsMinRoleCanKickDefault),
-            entry("ClanSettings.MinRoleCanAccept", ClanSettingsMinRoleCanAcceptDefault),
-            entry("ClanSettings.DefaultJoinRole", ClanSettingsDefaultJoinRoleDefault),
-
-            entry("DefaultClanSettings.FirstClanMoney", DefaultClanSettingsFirstClanMoneyDefault),
-            entry("DefaultClanSettings.MoneyToCreateClan", DefaultClanSettingsMoneyToCreateDefault),
-            entry("DefaultClanSettings.StartClanLevel", DefaultClanSettingsStartClanLevelDefault),
-            entry("DefaultClanSettings.ClanLevelsColors", ClanLevelsColorsDefault)
-    );
-
-
 }
---
+++ src/main/java/io/github/lofrol/UselessClan/Configurations/ClanConfigConfiguration.java
@@ -0,0 +1,151 @@
+package io.github.lofrol.UselessClan.Configurations;
+
+import io.github.lofrol.UselessClan.ClanObjects.EClanRole;
+import org.bukkit.configuration.MemoryConfiguration;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.checkerframework.checker.nullness.qual.Nullable;
+
+import java.util.List;
+import java.util.Map;
+
+public class ClanConfigConfiguration extends YamlConfiguration {
+    public ClanConfigConfiguration() {
+        set("Localization", LocalizationDefault);
+        set("NeedCalculateClanLevels", NeedCalculateClanLevelsDefault);
+
+        set("Delay.ClanBackup", DelayClanBackupDefault);
+        set("Delay.AutoSave", DelayAutoSaveDefault);
+        set("Delay.CalculateClanLevel", DelayCalculateClanLevelDefault);
+        set("Delay.CalculateClansTop", DelayCalculateClansTopDefault);
+
+        set("ClanSettings.MinRoleCanSethome", ClanSettingsMinRoleCanSethomeDefault);
+        set("ClanSettings.MinRoleCanWithdraw", ClanSettingsMinRoleCanWithdrawDefault);
+        set("ClanSettings.MinRoleCanKick", ClanSettingsMinRoleCanKickDefault);
+        set("ClanSettings.MinRoleCanAccept", ClanSettingsMinRoleCanAcceptDefault);
+        set("ClanSettings.DefaultJoinRole", ClanSettingsDefaultJoinRoleDefault);
+
+        set("DefaultClanSettings.FirstClanMoney", DefaultClanSettingsFirstClanMoneyDefault);
+        set("DefaultClanSettings.MoneyToCreateClan", DefaultClanSettingsMoneyToCreateDefault);
+        set("DefaultClanSettings.StartClanLevel", DefaultClanSettingsStartClanLevelDefault);
+        set("DefaultClanSettings.ClanLevelsColors", ClanLevelsColorsDefault);
+    }
+
+    public static @Nullable ClanConfigConfiguration tryLoadDifferentConfig(FileConfiguration tempConfig) {
+        var tempThis = new ClanConfigConfiguration(tempConfig);
+
+        tempThis.LocalizationKey = tempConfig.getString("Localization");
+        if (tempThis.LocalizationKey == null) return null;
+
+        String tempNeedCalculateClanLevels = tempConfig.getString("NeedCalculateClanLevels");
+        if (tempNeedCalculateClanLevels == null) return null;
+        tempThis.NeedCalculateClanLevels = Boolean.parseBoolean(tempNeedCalculateClanLevels);
+
+        String tempBackupDelay = tempConfig.getString("Delay.ClanBackup");
+        if (tempBackupDelay == null) return null;
+        tempThis.backupDelay = Integer.parseInt(tempBackupDelay);
+
+        String tempAutoSaveDelay  = tempConfig.getString("Delay.AutoSave");
+        if (tempAutoSaveDelay  == null) return null;
+        tempThis.autoSaveDelay = Integer.parseInt(tempAutoSaveDelay);
+
+        String tempCalcClanLvlDelay = tempConfig.getString("Delay.CalculateClanLevel");
+        if (tempCalcClanLvlDelay  == null) return null;
+        tempThis.calcClanLvlDelay = Integer.parseInt(tempCalcClanLvlDelay);
+
+        String tempCalcClanTopDelay = tempConfig.getString("Delay.CalculateClanLevel");
+        if (tempCalcClanTopDelay  == null) return null;
+        tempThis.calcClanTopDelay = Integer.parseInt(tempCalcClanTopDelay);
+
+        tempThis.MinRoleCanSethome = EClanRole.fromInt(tempConfig.getInt("ClanSettings.MinRoleCanSethome"));
+        if (tempThis.MinRoleCanSethome == EClanRole.NONE) return null;
+        tempThis.MinRoleCanWithdraw = EClanRole.fromInt(tempConfig.getInt("ClanSettings.MinRoleCanWithdraw"));
+        if (tempThis.MinRoleCanWithdraw == EClanRole.NONE) return null;
+        tempThis.MinRoleCanKick = EClanRole.fromInt(tempConfig.getInt("ClanSettings.MinRoleCanKick"));
+        if (tempThis.MinRoleCanKick == EClanRole.NONE) return null;
+        tempThis.MinRoleCanAccept = EClanRole.fromInt(tempConfig.getInt("ClanSettings.MinRoleCanAccept"));
+        if (tempThis.MinRoleCanAccept == EClanRole.NONE) return null;
+        tempThis.DefaultJoinRole = EClanRole.fromInt(tempConfig.getInt("ClanSettings.DefaultJoinRole"));
+        if (tempThis.DefaultJoinRole == EClanRole.NONE) return null;
+
+        String tempClanMoney = tempConfig.getString("DefaultClanSettings.FirstClanMoney");
+        if (tempClanMoney == null) return null;
+        tempThis.FirstClanMoney = Double.parseDouble(tempClanMoney);
+
+        String tempMoneyToCreate = tempConfig.getString("DefaultClanSettings.MoneyToCreateClan");
+        if (tempMoneyToCreate == null) return null;
+        tempThis.MoneyToCreate = Double.parseDouble(tempMoneyToCreate);
+
+        String tempStartClanLevel = tempConfig.getString("DefaultClanSettings.StartClanLevel");
+        if (tempStartClanLevel == null) return null;
+        tempThis.StartClanLevel = Integer.parseInt(tempStartClanLevel);
+
+        tempThis.ClanLevelsColors = tempConfig.getStringList("DefaultClanSettings.ClanLevelsColors");
+        if (tempThis.ClanLevelsColors.size() < 1) return null;
+
+        return tempThis;
+    }
+
+    private ClanConfigConfiguration(FileConfiguration tempConfig) {
+        setDefaults(tempConfig);
+    }
+
+
+    private String LocalizationKey          = LocalizationDefault;
+    private boolean NeedCalculateClanLevels = NeedCalculateClanLevelsDefault;
+    private int backupDelay                 = DelayClanBackupDefault;
+    private int autoSaveDelay               = DelayAutoSaveDefault;
+    private int calcClanLvlDelay            = DelayCalculateClanLevelDefault;
+    private int calcClanTopDelay            = DelayCalculateClansTopDefault;
+    private EClanRole MinRoleCanSethome     = EClanRole.fromInt(ClanSettingsMinRoleCanSethomeDefault);
+    private EClanRole MinRoleCanWithdraw    = EClanRole.fromInt(ClanSettingsMinRoleCanWithdrawDefault);
+    private EClanRole MinRoleCanKick        = EClanRole.fromInt(ClanSettingsMinRoleCanKickDefault);
+    private EClanRole MinRoleCanAccept      = EClanRole.fromInt(ClanSettingsMinRoleCanAcceptDefault);
+    private EClanRole DefaultJoinRole       = EClanRole.fromInt(ClanSettingsDefaultJoinRoleDefault);
+    private double FirstClanMoney           = DefaultClanSettingsFirstClanMoneyDefault;
+    private double MoneyToCreate            = DefaultClanSettingsMoneyToCreateDefault;
+    private int StartClanLevel              = DefaultClanSettingsStartClanLevelDefault;
+    private List<String> ClanLevelsColors   = ClanLevelsColorsDefault;
+
+
+    /*
+    *   Getters section
+    * */
+    public String getLocalizationKey() { return LocalizationKey; }
+    public boolean isNeedCalculateClanLevels() { return NeedCalculateClanLevels; }
+    public int getBackupDelay() { return backupDelay; }
+    public int getAutoSaveDelay() { return autoSaveDelay; }
+    public int getCalcClanLvlDelay() { return calcClanLvlDelay; }
+    public int getCalcClanTopDelay() { return calcClanTopDelay; }
+    public EClanRole getMinRoleCanSethome() { return MinRoleCanSethome; }
+    public EClanRole getMinRoleCanWithdraw() { return MinRoleCanWithdraw; }
+    public EClanRole getMinRoleCanKick() { return MinRoleCanKick; }
+    public EClanRole getMinRoleCanAccept() { return MinRoleCanAccept; }
+    public EClanRole getDefaultJoinRole() { return DefaultJoinRole; }
+    public double getFirstClanMoney() { return FirstClanMoney; }
+    public double getMoneyToCreate() { return MoneyToCreate; }
+    public int getStartClanLevel() { return StartClanLevel; }
+    public List<String> getClanLevelsColors() { return ClanLevelsColors; }
+
+
+
+    /*
+    *   Section of default values
+    * */
+    public static final String LocalizationDefault = "en-US";
+    public static final boolean NeedCalculateClanLevelsDefault = true;
+    public static final int DelayClanBackupDefault = 864000;
+    public static final int DelayAutoSaveDefault = 24000;
+    public static final int DelayCalculateClanLevelDefault = 12000;
+    public static final int DelayCalculateClansTopDefault = 12000;
+    public static final int ClanSettingsMinRoleCanSethomeDefault = 4;
+    public static final int ClanSettingsMinRoleCanWithdrawDefault = 2;
+    public static final int ClanSettingsMinRoleCanKickDefault = 3;
+    public static final int ClanSettingsMinRoleCanAcceptDefault = 3;
+    public static final int ClanSettingsDefaultJoinRoleDefault = 1;
+    public static final double DefaultClanSettingsFirstClanMoneyDefault = 0;
+    public static final double DefaultClanSettingsMoneyToCreateDefault = 10000;
+    public static final int DefaultClanSettingsStartClanLevelDefault = 0;
+    public static final List<String> ClanLevelsColorsDefault =
+            List.of("&f", "&a", "&2", "&3", "&9", "&1", "&e", "&6", "&d", "&5", "&0");
+}
